b102c9a2ea5915a4a5afa10ed8bff011d18b45f6
gewgew4 <29262845+gewgew4@users.noreply.github.com>
2025-06-03 22:49:15 -0300

    * Added some folders * Added (for now) Common layer

---
+++ Api/Controllers/PermissionsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class PermissionsController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
